114be0b29ac831f7cf213186c64acce4dc394519
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-01 00:23:10 +0530

    Added : Fibonacci Series

--- src/FibonacceSeries.java
+++ src/FibonacceSeries.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class FibonacceSeries {
+import java.util.Scanner;
+
+public class FibonacceSeries {
+    public static void main(String[] args) {
+        //Initializing
+        int a=0,b=1,sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of terms n: ");
+        // Input int,used nextint
+        int n = sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println(a);
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+    }
 }
--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class StopWatch {
+import java.util.Scanner;
+/*Coupon Numbers
+Desc -> Given N distinct Coupon Numbers, how many random numbers do you
+need to generate a distinct coupon number? This program simulates this random
+process.
+b. I/P -> N Distinct Coupon Number
+c. Logic -> repeatedly choose a random number and check whether it's a new one.
+d. O/P -> total random number needed to have all distinct numbers.
+e. Functions => Write Class Static Functions to generate random numbers and to
+process distinct coupons.*/
+public class StopWatch {
+    public static void main(String[] args)
+    {
+        //Variable Declration
+        long start = 0, stop = 0;
+
+        //Scanner object for user input
+        Scanner sc = new Scanner(System.in);
+
+        //Showing Stopwatch options to user
+        while(true) {
+            System.out.println("1. Start StopWatch \n2. Stop StopWatch \n3. Exit");
+
+            System.out.println("Enter your Choice :");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    //Getting the Starting Time of Stopwatch from system in nanoseconds
+                    start = System.nanoTime();
+                    System.out.println("Start Time :" + start);
+                    break;
+                case 2:
+                    //Calculating the Seconds to be lapsed by starting and ending time
+                    stop = System.nanoTime();
+                    long sec = ((stop - start) / 1000000000);
+                    System.out.println("edn Time :" + stop);
+                    int min = (int) sec / 60;
+                    int hr = min / 60;
+                    System.out.println("Elapsed Time is " + hr + ":" + min + ":" + sec);
+                    break;
+            }
+
+            if(choice >= 3) {
+                System.out.println("Closing the Stopwatch.");
+                break;
+            }
+        }
+    }
 }
+
