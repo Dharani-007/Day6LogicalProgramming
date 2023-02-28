1ef371db3628c7dd73d49d99807b1ee8b57e7bdc
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-01 02:21:45 +0530

    Added : Stop Watch

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,42 +1,18 @@
-import java.util.Scanner;
+import java.util.Random;
+import java.util.*;
+
 public class StopWatch {
-    public static void main(String[] args)
-    {
-        //Variable Declration
-        long start = 0, stop = 0;
 
-        //Scanner object for user input
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Stop Watch Program");
         Scanner sc = new Scanner(System.in);
-
-        //Showing Stopwatch options to user
-        while(true) {
-            System.out.println("1. Start StopWatch \n2. Stop StopWatch \n3. Exit");
-
-            System.out.println("Enter your Choice :");
-            int choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    //Getting the Starting Time of Stopwatch from system in nanoseconds
-                    start = System.nanoTime();
-                    System.out.println("Start Time :" + start);
-                    break;
-                case 2:
-                    //Calculating the Seconds to be lapsed by starting and ending time
-                    stop = System.nanoTime();
-                    long sec = ((stop - start) / 1000000000);
-                    System.out.println("edn Time :" + stop);
-                    int min = (int) sec / 60;
-                    int hr = min / 60;
-                    System.out.println("Elapsed Time is " + hr + ":" + min + ":" + sec);
-                    break;
-            }
-
-            if(choice >= 3) {
-                System.out.println("Closing the Stopwatch.");
-                break;
-            }
-        }
+        System.out.println("Enter a key to start the time");
+        char startch = sc.next().charAt(0);
+        Long start = System.currentTimeMillis();
+        System.out.println("Enter a key to stop the time");
+        char stopch = sc.next().charAt(0);
+        Long stop = System.currentTimeMillis();
+        float timeElapsed = (float) (stop - start) / 1000;
+        System.out.println("Time = " + timeElapsed + " Seconds");
     }
 }
-
